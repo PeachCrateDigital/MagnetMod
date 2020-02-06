49bd40840a7d9600f240a069199de02729b85ca8
Daniel Peach <dpeaches96@gmail.com>
2020-02-06 15:43:15 -0500

    Loaded Mod

---
+++ src/main/java/io/peachapps/magnetized/Magnetized.java
@@ -0,0 +1,8 @@
+package io.peachapps.magnetized;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Magnetized.MODID)
+public class Magnetized {
+    public static final String MODID = "magnetized";
+}
